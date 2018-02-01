7274c60310d5c3d609004d9b6e77c6d5a1e788e7
Chicloon <Chicloon@gmail.com>
2018-02-01 15:44:16 +0900

    4.50

---
+++ src/containers/book-list.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import { selectBook } from "../actions/index";
+
+class BookList extends Component {
+  renderList = () =>
+    this.props.books.map(book => (
+      <li
+        onClick={() => this.props.selectBook(book)}
+        key={book.title}
+        className="list-group-item"
+      >
+        {book.title}
+      </li>
+    ));
+
+  render() {
+    return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
